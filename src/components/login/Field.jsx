2714681605085bc041d797d4835291208387699f
Donny <donny1103@hotmail.com>
2019-11-07 22:21:32 -0500

    refactor Login with styled-components

---
+++ src/components/login/Field.jsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// props: type, error, value, onChange, placeholder ...rest
+
+const Wrapper = styled.div`
+  position: relative;
+  margin: 20px 0;
+  &::after{
+    display: ${props => props.isInvalid ? 'block' : 'none'};
+    font-family: "Font Awesome 5 Pro";
+    font-weight: 900;
+    content: '\f069';
+    position: absolute;
+    right: 0;
+    font-size: 15px;
+    color: #dc3545;
+    transform: translate(-70%,-150%);
+  }
+`;
+
+const Label = styled.div`
+  font-weight: bold;
+`;
+
+const Input = styled.input`
+  display: block;
+  width: 100%;
+  height: calc(1.5em + .75rem + 2px);
+  padding: .375rem .75rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid ${props => props.isInvalid ? '#dc3545' : '#ced4da'};
+  border-radius: .25rem;
+  transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+  &:focus{
+    outline: 0;
+    color: #495057;
+    background-color: #fff;
+    border-color: ${props => props.isInvalid ? '#dc3545' : '#80bdff'};
+  }
+`;
+
+const Error = styled.div`
+  position: absolute;
+  margin-top: 0;
+  width: 100%;
+  font-size: 12px;
+  color: #dc3545;
+`
+
+function Field ({ label, type, error, value, onChange, placeholder, ...rest }) {
+  const isInvalid = Boolean(error);
+  return (
+    <Wrapper isInvalid={isInvalid}>
+      {Boolean(label) && <Label>{label}</Label>}
+      <Input type={type} value={value} onChange={onChange} placeholder={placeholder} {...rest} isInvalid={isInvalid} />
+      {Boolean(error) && <Error>{error}</Error>}
+    </Wrapper>
+  )
+}
+
+export default Field;
+
+export const Title = styled.div`
+  text-align: center;
+`
+
+export const ForgetPass = styled.button`
+    background: transparent;
+    border: none;
+    font-size: 14px;
+    position: absolute;
+    right: 0;
+    transform: translate(-40%, -20px);
+
+    &:focus{
+      outline: none;
+    }
+
+    &:hover{
+      color: #0056b3;
+    }
+`
+
+export const Button = styled.button`
+    display: ${props => props.block !== undefined ? 'block' : 'inline-block'};
+    width: ${props => props.block !== undefined ? '100%' : ''};
+    font-weight: 400;
+    text-align: center;
+    vertical-align: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    color: #fff;
+    background-color: #007bff;
+    border-color: #007bff;
+    border: 1px solid transparent;
+    padding: .375rem .75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: .25rem;
+    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    &:focus{
+      outline: none;
+    }
+    &:hover{
+      background-color: #0069d9;
+      border-color: #0062cc;
+    }
+`
+
+export const Form = styled.form`
+  max-width: 600px; 
+  margin: auto;
+  padding: 50px;
+  position: relative;
+`;
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import classnames from "classnames";
+import Field, { Title, ForgetPass, Button, Form } from './Field'
 
-function Login() {
+function Login () {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
@@ -31,73 +31,50 @@ function Login() {
     if (Object.keys(errors).length) setErrors({});
   }, [password, email]);
 
-  return (
-    <form
-      onSubmit={handleSubmit}
-      style={{ maxWidth: 600, margin: "auto", padding: 50 }}
-      noValidate
-    >
-      <div className="text-center">
-        <h3>U of Tears Login</h3>
-      </div>
-      <div
-        className="form-group mt-3"
-        style={{ position: "relative", marginBottom: 30 }}
-      >
-        <label>Email</label>
-        <div>
-          <input
-            type="email"
-            className={classnames({
-              "form-control": true,
-              "is-invalid": !!errors.email
-            })}
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-          <div
-            className="invalid-feedback mt-0"
-            style={{ position: "absolute" }}
-          >
-            {errors.email}
-          </div>
-        </div>
-      </div>
+  const emailProps = {
+    type: "email",
+    value: email,
+    error: errors.email,
+    placeholder: "Email",
+    onChange: e => setEmail(e.target.value),
+  }
 
-      <div
-        className="form-group"
-        style={{ position: "relative", marginBottom: 30 }}
-      >
-        <label>Password</label>
-        <div>
-          <input
-            type="password"
-            className={classnames({
-              "form-control": true,
-              "is-invalid": !!errors.password
-            })}
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-          <div
-            className="invalid-feedback mt-0"
-            style={{ position: "absolute" }}
-          >
-            {errors.password}
-          </div>
-        </div>
-      </div>
-      <div className="text-center" style={{ marginTop: 40 }}>
-        <button type="submit" className="btn btn-primary btn-block">
-          Login
-        </button>
-      </div>
-      <div className="text-center mt-3">
-        <button type="button" className="btn btn-link">
-          forgot password
-        </button>
-      </div>
-    </form>
+  const passwordProps = {
+    type: "password",
+    value: password,
+    error: errors.password,
+    placeholder: "Password",
+    onChange: e => setPassword(e.target.value),
+  }
+
+  return (
+    <Form onSubmit={handleSubmit} noValidate>
+      <Title>
+        <h3>Welcome to U of Tears</h3>
+      </Title>
+
+      <Field {...emailProps} />
+
+      <Field {...passwordProps} />
+
+      <ForgetPass type="button">
+        Forgot password?
+      </ForgetPass>
+
+      <Button style={{ marginTop: 40 }} block>
+        Login
+      </Button>
+
+      <div style={{ textAlign: 'center', margin: '10px' }}>OR</div>
+
+      <Button type="button" style={{ margin: '10px 0' }} block>
+        Continue With Facebook
+      </Button>
+      <Button type="button" block>
+        Continue With Google
+      </Button>
+
+    </Form>
   );
 }
 
